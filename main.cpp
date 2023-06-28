6ff07ebce3113bda4015950def44113b5e96e679
Huawei Li <huaweil@huaweil-lt.nvidia.com>
2023-06-28 10:47:18 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     int arr[5] = {3, 1, 2, 5, 4};
-    quickSort(arr, 0, 5);
+    quickSort(arr, 0, 4);
     for(int i = 0; i < 5; i++)
     {
         std::cout << arr[i] << " ";
--- test.cpp
+++ test.cpp
@@ -9,13 +9,13 @@ void quickSort(int* arr, int left, int right)
 {
     if (left < 0 || right < 0) return;
     if (left >= right) return;
-    int midIdx = (left + right) >> 1, start = left, end = right - 1;
+    int midIdx = (left + right) >> 1, start = left - 1, end = right + 1;
     while (start < end)
     {
-        while(arr[start] <= arr[midIdx]) start++;
-        while(arr[end] <= arr[midIdx]) end--;
+        do start++; while(arr[start] < arr[midIdx]);
+        do end--; while(arr[end] > arr[midIdx]);
         if (start < end) swap(arr, start, end);
     }
-    quickSort(arr, left, start);
-    quickSort(arr, start, right);
+    quickSort(arr, left, end);
+    quickSort(arr, end+1, right);
 }
